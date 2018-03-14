c8342fa773ec2a97d795d849aafd134e034d9bbb
Simon Hudon <simon.hudon@gmail.com>
2018-03-14 16:39:14 -0400

    [WIP] moved `monotonicity` over from `unitb/lean-lib`

--- meta/expr.lean
+++ meta/expr.lean
@@ -37,6 +37,13 @@ e.fold [] (λ e' _ es, if e'.is_local_constant ∧ ¬ e' ∈ es then e' :: es el
 meta def expr.list_meta_vars (e : expr) : list expr :=
 e.fold [] (λ e' _ es, if e'.is_meta_var ∧ ¬ e' ∈ es then e' :: es else es)
 
+meta def expr.is_mvar : expr → bool
+ | (expr.mvar _ _ _) := tt
+ | _ := ff
+
+meta def expr.list_meta_vars (e : expr) : list expr :=
+e.fold [] (λ e' _ es, if expr.is_mvar e' ∧ ¬ e' ∈ es then e' :: es else es)
+
 end expr
 
 namespace tactic
