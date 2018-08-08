366bd52352129ec2ecc2b3b9e32608f86a2b9032
Simon Hudon <simon.hudon@gmail.com>
2018-08-08 02:53:35 -0400

    feat(tactic/mono): update traversable

--- meta/expr.lean
+++ meta/expr.lean
@@ -41,9 +41,6 @@ meta def expr.is_mvar : expr → bool
  | (expr.mvar _ _ _) := tt
  | _ := ff
 
-meta def expr.list_meta_vars (e : expr) : list expr :=
-e.fold [] (λ e' _ es, if expr.is_mvar e' ∧ ¬ e' ∈ es then e' :: es else es)
-
 end expr
 
 namespace tactic
